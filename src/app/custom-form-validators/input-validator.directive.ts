b9ff5847a35d3c8998d287a9d54e961061e44f30
tluwaniMS <tluwani2020@gmail.com>
2021-12-02 07:34:27 +0200

    adding-proper-names-custom-validation

--- src/app/custom-form-validators/input-validator.directive.ts
+++ src/app/custom-form-validators/input-validator.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 import { AbstractControl, Validator, NG_VALIDATORS } from '@angular/forms';
+import { invalidNames } from 'src/app/sample-data/sample-data';
 
 @Directive({
   selector: '[appInputValidator]',
@@ -13,7 +14,7 @@ import { AbstractControl, Validator, NG_VALIDATORS } from '@angular/forms';
 })
 export class InputValidatorDirective implements Validator {
   validate(control: AbstractControl): { [key: string]: any } | null {
-    if (control.value === 'ben') {
+    if (invalidNames.includes(control.value)) {
       return { phoneNumberInvalid: true };
     }
     return null;
---
+++ src/app/sample-data/sample-data.ts
@@ -0,0 +1,1 @@
+export const invalidNames = ['admin', 'administrator'];
