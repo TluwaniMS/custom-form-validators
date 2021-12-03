4f2f0b3ab4ff7b55e5c2c4f59c8705329ea56bdc
tluwaniMS <tluwani2020@gmail.com>
2021-12-03 08:14:13 +0200

    adding-regex-for-password-checks

--- src/app/custom-form-validators/password-confirmation-validator.directive.ts
+++ src/app/custom-form-validators/password-confirmation-validator.directive.ts
@@ -1,8 +1,27 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validator,
+  NG_VALIDATORS,
+} from '@angular/forms';
 
 @Directive({
   selector: '[appPasswordConfirmationValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: PasswordConfirmationValidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class PasswordConfirmationValidatorDirective {
-  constructor() {}
+export class PasswordConfirmationValidatorDirective implements Validator {
+  validate(control: AbstractControl): { [key: string]: any } | null {
+    if (control.value) {
+      return { nameIsInvalid: true };
+    }
+    return null;
+  }
 }
--- src/app/sample-data/sample-data.ts
+++ src/app/sample-data/sample-data.ts
@@ -1,1 +1,10 @@
 export const invalidNames = ['admin', 'administrator'];
+
+export const specialCharactersRegEx =
+  /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+export const lowerCaseLettersRegEx = /[a-z]+/;
+
+export const upperCaseLettersRegEx = /[A-Z]+/;
+
+export const numericsRegEx = /[0-9]+/;
