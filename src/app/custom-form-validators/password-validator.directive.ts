66acdb5919a5aa27b6acdac1fbe6a49ba8df3345
tluwaniMS <tluwani2020@gmail.com>
2021-12-03 08:24:49 +0200

    adding-fix-to-password-validator

--- src/app/custom-form-validators/password-validator.directive.ts
+++ src/app/custom-form-validators/password-validator.directive.ts
@@ -1,8 +1,27 @@
 import { Directive } from '@angular/core';
+import {
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validator,
+  NG_VALIDATORS,
+} from '@angular/forms';
 
 @Directive({
-  selector: '[appPasswordValidator]',
+  selector: '[appPasswordConfirmationValidator]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: PasswordValidatorDirective,
+      multi: true,
+    },
+  ],
 })
-export class PasswordValidatorDirective {
-  constructor() {}
+export class PasswordValidatorDirective implements Validator {
+  validate(control: AbstractControl): { [key: string]: any } | null {
+    if (control.value) {
+      return { nameIsInvalid: true };
+    }
+    return null;
+  }
 }
