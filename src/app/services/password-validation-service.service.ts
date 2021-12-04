7d51cd57c3a0b6d459f186f562ede93dd14c6f1e
tluwaniMS <tluwani2020@gmail.com>
2021-12-04 13:02:33 +0200

    adding-service-logic-for-password-validation

--- src/app/services/password-validation-service.service.ts
+++ src/app/services/password-validation-service.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {
+  specialCharactersRegEx,
+  lowerCaseLettersRegEx,
+  upperCaseLettersRegEx,
+  numericsRegEx,
+} from 'src/app/sample-data/sample-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PasswordValidationService {
   constructor() {}
+
+  validatePassword(password: string) {}
+
+  validatePasswordLength(password: string) {}
 }
