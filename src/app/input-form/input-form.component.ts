9d9cb46dd186bda383f0d446037ac837274bef32
tluwaniMS <tluwani2020@gmail.com>
2021-12-04 13:57:12 +0200

    adding-password-validator-directive-to-reactive-form

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InputValidatorDirective } from 'src/app/custom-form-validators/input-validator.directive';
+import { PasswordValidatorDirective } from 'src/app/custom-form-validators/password-validator.directive';
 
 @Component({
   selector: 'app-input-form',
@@ -12,7 +13,8 @@ export class InputFormComponent implements OnInit {
 
   constructor(
     public fb: FormBuilder,
-    private inputValidatorDirective: InputValidatorDirective
+    private inputValidatorDirective: InputValidatorDirective,
+    private passwordValidatorDirective: PasswordValidatorDirective
   ) {}
 
   ngOnInit(): void {
@@ -32,7 +34,13 @@ export class InputFormComponent implements OnInit {
         ]),
       ],
       email: ['', Validators.compose([Validators.email, Validators.required])],
-      password: ['', Validators.compose([Validators.required])],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          this.passwordValidatorDirective.validate,
+        ]),
+      ],
       passWordConfirmation: ['', Validators.compose([Validators.required])],
     });
   }
