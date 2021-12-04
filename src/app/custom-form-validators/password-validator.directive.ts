9e79c12b43b2548181a4053eb2174bb47744890d
tluwaniMS <tluwani2020@gmail.com>
2021-12-04 13:29:53 +0200

    adding-final-pieces-to-password-custom-validator

--- src/app/custom-form-validators/password-validator.directive.ts
+++ src/app/custom-form-validators/password-validator.directive.ts
@@ -1,11 +1,6 @@
 import { Directive } from '@angular/core';
-import {
-  AbstractControl,
-  ValidationErrors,
-  ValidatorFn,
-  Validator,
-  NG_VALIDATORS,
-} from '@angular/forms';
+import { AbstractControl, Validator, NG_VALIDATORS } from '@angular/forms';
+import { PasswordValidationService } from 'src/app/services/password-validation-service.service';
 
 @Directive({
   selector: '[appPasswordConfirmationValidator]',
@@ -18,10 +13,16 @@ import {
   ],
 })
 export class PasswordValidatorDirective implements Validator {
+  constructor(private passwordValidationService: PasswordValidationService) {}
   validate(control: AbstractControl): { [key: string]: any } | null {
-    if (control.value) {
-      return { nameIsInvalid: true };
-    }
-    return null;
+    const passwordValuesAreSatisfied =
+      this.passwordValidationService.validatePassword(control.value);
+    const passwordLengthIsSatisfied =
+      this.passwordValidationService.validatePasswordLength(control.value);
+
+    const passwordIsValid =
+      passwordValuesAreSatisfied && passwordLengthIsSatisfied;
+
+    return passwordIsValid ? { passwordIsValidated: true } : null;
   }
 }
